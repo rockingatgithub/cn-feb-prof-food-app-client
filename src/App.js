b324b31981de9b33589105b83a7360c438e25cf9
singhzrepo <contactwithsid@gmail.com>
2022-10-30 21:13:51 +0530

    Initialized client changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+function App(props) {
+
+  const fetchCall = () => {
+    fetch('http://localhost:8000/test')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <button onClick={fetchCall} >API test</button>
+      
     </div>
   );
 }
